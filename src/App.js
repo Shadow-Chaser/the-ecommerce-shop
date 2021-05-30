8bf72d7528e3d37a771445fd7ced879831abc0d8
arpan-jo <arpanjorng@gamil.com>
2021-05-30 21:35:40 +0600

    added checkout

--- src/App.js
+++ src/App.js
@@ -1,24 +1,24 @@
-import React from 'react'
+import React from 'react';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './App.css';
-import {
-  BrowserRouter as Router,
-  Switch,
-  Route,
-} from "react-router-dom";
-import Header from './components/Header/Header'
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
+import Header from './components/Header/Header';
 import Home from './components/Home/Home';
+import Checkout from './components/Checkout';
 function App() {
-  return (
-    <Router>
-    <Header></Header>
-      <Switch>
-        <Route exact path="/">
-        <Home></Home>
-        </Route>
-      </Switch>
-    </Router>
-  );
+   return (
+      <Router>
+         <Header></Header>
+         <Switch>
+            <Route exact path="/">
+               <Home></Home>
+            </Route>
+            <Route path="checkout">
+               <Checkout></Checkout>
+            </Route>
+         </Switch>
+      </Router>
+   );
 }
 
 export default App;
--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -3,14 +3,17 @@ import { useDispatch, useSelector } from 'react-redux';
 import { removeFromCart } from '../../redux/actions/cartAction';
 import { Link } from 'react-router-dom';
 import { Button, Form } from 'react-bootstrap';
+import Fade from 'react-reveal/Fade';
+import Modal from 'react-modal';
+import Checkout from '../Checkout';
 
 const Cart = () => {
    const [procedBol, setProcedBol] = useState(false);
+   // const [input, setInput] = useState('');
 
    const cart = useSelector(state => {
       return state.product.cart;
    });
-
 
    const dispatch = useDispatch();
 
@@ -23,6 +26,23 @@ const Cart = () => {
       setProcedBol(bol);
    };
 
+   // const handleInput = e => {
+   //    setInput(e.target.value);
+   // };
+   // const orderCheckout = e => {
+   //    e.preventDefault();
+   // };
+
+   // const handleClick = () => {};
+
+   const [modalIsOpen, setIsOpen] = useState(false);
+   function openModal() {
+      setIsOpen(true);
+   }
+
+   function closeModal() {
+      setIsOpen(false);
+   }
    return (
       <div>
          <h1>Cart</h1>
@@ -40,46 +60,36 @@ const Cart = () => {
                <p>
                   ${p.price} x {p.count}{' '}
                </p>
-               <Button onClick={() => handleProced(!false)}>Proced</Button>
             </div>
          ))}
-
+         <Button onClick={() => handleProced(!false)}>Proced</Button>
          {cart.length > 0 && <h2>Total :$ {total}</h2>}
 
-          {procedBol && (
-                <Fade right cascade>
-                  <div>
-                      <Form> <label>Email</label>
-                          <input
-                            name="email"
-                            type="email"
-                            required
-      
-                          ></input>
+         {procedBol && (
+            <Fade right cascade>
+               <div>
+                  <Form>
+                     <label>Email</label>
+                     <input name="email" type="email" required></input>
+                     <label>Name</label>
+                     <input name="name" type="text" required></input>
+                     <label>Address</label>
+                     <input name="address" type="text" required></input>
 
-                          <label>Name</label>
-                          <input
-                            name="name"
-                            type="text"
-                            required
- 
-                          ></input>
-                          <label>Address</label>
-                          <input
-                            name="address"
-                            type="text"
-                            required
-
-                          ></input>
-                          <Button className="button primary" type="submit">
-                            Checkout
-                          </Button></Form>
-     
-                  </div>
-                </Fade>
-              )}
-            </div>
-          )}
+                     <Button
+                        onClick={() => openModal()}
+                        className="button primary"
+                     >
+                        Checkout
+                     </Button>
+                  </Form>
+               </div>
+            </Fade>
+         )}
+         <Checkout>
+            modalIsOpen={modalIsOpen}
+            closeModal={closeModal}
+         </Checkout>
       </div>
    );
 };
